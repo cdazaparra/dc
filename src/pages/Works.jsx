18f8cb0d8c65e5d929a4289b8536a9b9396551ef
Andres Daza <cdazaparra@gmail.com>
2023-04-05 14:02:46 -0500

    gh-pages

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <div className="mt-20">
+      <h1 className="text-6xl text-center">Trabajos</h1>
+    </div>
+  );
 };
 
 export default Works;
