e0e794ac916175b98c0ad2a5cde0b7d1fcfd6e40
Andres Daza <cdazaparra@gmail.com>
2023-04-18 17:32:13 -0500

    change on routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
         errorElement: <NotFound></NotFound>,
         children: [
           {
-            path: "/",
+            path: "/dc",
             element: <Home></Home>
           },
           {
